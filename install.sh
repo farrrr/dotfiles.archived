505ae826ddbfc68a302b89e5bd14050b44dc0d9e
Far Tseng <farrrr@gmail.com>
2014-08-25 11:02:08 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,67 +1,73 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-warn() {
-    echo "$1" >&2
+## setup parameters
+app_name='dotfiles'
+app_dir="$HOME/.dotfiles"
+
+## basic setup tools
+msg() {
+    printf '%b\n' "$1" >&2
 }
 
-die() {
-    warn "$1"
+success() {
+    if [ "$ret" -eq '0' ]; then
+        msg "\e[32m[✔]\e[0m ${1}${2}"
+    fi
+}
+
+error() {
+    msg "\e[31m[✘]\e[0m ${1}${2}"
     exit 1
 }
 
+debug() {
+    if [ "$debug_mode" -eq '1' ] && [ "$ret" -gt '1' ]; then
+        msg "An error occurred in function \"${FUNCNAME[$i+1]}\" on line ${BASH_LINENO[$i+1]}, we're sorry for that."
+    fi
+}
+
+program_exists() {
+    local ret='0'
+    type $1 >/dev/null 2>&1 || { local ret='1'; }
+
+    # throw error on non-zero return value
+    if [ ! "$ret" -eq '0' ]; then
+        error "$2"
+    fi
+}
+
+## setup functions
 lnif() {
-    if [ ! -e $2 ] ; then
-        ln -s $1 $2
+    if [ ! -e "$2" ]; then
+        ln -s "$1" "$2"
     fi
-    if [ -L $2 ] ; then
-        if [ -d $2 ] ; then
-            rm $2
+
+    if [ -L "$2" ]; then
+        if [ -d "$2" ]; then
+            rm "$2"
         fi
-        ln -sf $1 $2
+        ln -sf "$1" "$2"
+    fi
+
+    ret="$?"
+    debug
+}
+
+do_backup() {
+    if [ -e "$2" ]; then
+        today=`date +%Y%m%d_%s`
+        [ -e "$2" ] && [ ! -L "$2" ] && mv "$2" "$2.today";
+        ret="$?"
+        success "$1"
+        debug
     fi
 }
 
-readFileLink() {
-    TARGET_FILE=$1
-
-    cd `dirname $TARGET_FILE`
-    TARGET_FILE=`basename $TARGET_FILE`
-
-    # Iterate down a (possible) chain of symlinks
-    while [ -L "$TARGET_FILE" ]
-    do
-        TARGET_FILE=`readlink $TARGET_FILE`
-        cd `dirname $TARGET_FILE`
-        TARGET_FILE=`basename $TARGET_FILE`
-    done
-
-    # Compute the canonicalized name by finding the physical path 
-    # for the directory we're in and appending the target file.
-    PHYS_DIR=`pwd -P`
-    RESULT=$PHYS_DIR/$TARGET_FILE
-    echo $RESULT
-}
-
-echo 'Installing dotfiles'
-today=`date +%Y%m%d`
-
-for i in $HOME/.bashrc $HOME/.aliases $HOME/.bash_prompt $HOME/.bash_profile $HOME/.functions $HOME/.exports $HOME/.gitconfig $HOME/.zshrc $HOME/.osx $HOME/.screenrc $HOME/.tmux.conf; do [ -e $i ] && [ ! -L $i ] && mv $i $i.$today; done
-
-script="$(readFileLink $0)"
-dotfilepath="$(dirname $script)"
-
-echo 'setting up symlinks'
-lnif $dotfilepath/.bashrc $HOME/.bashrc
-lnif $dotfilepath/.bash_prompt $HOME/.bash_prompt
-lnif $dotfilepath/.bash_profile $HOME/.bash_profile
-lnif $dotfilepath/.aliases $HOME/.aliases
-lnif $dotfilepath/.exports $HOME/.exports
-lnif $dotfilepath/.functions $HOME/.functions
-lnif $dotfilepath/.zshrc $HOME/.zshrc
-lnif $dotfilepath/.screenrc $HOME/.screenrc
-lnif $dotfilepath/.gitconfig $HOME/.gitconfig
-lnif $dotfilepath/.tmux $HOME/.tmux
-lnif $dotfilepath/.tmux.conf $HOME/.tmux.conf
-lnif $dotfilepath/.osx $HOME/.osx
-
-echo "Install Done"
+## main()
+endpath="$app_dir"
+for settings in ".tmux" ".gitconfig" ".osx" ".screenrc" ".tmux.conf" ".zshrc"
+do
+    do_backup "$settings"
+    lnif "$endpath/$settings"   "$HOME/$settings"
+    success "creating symlink $settings"
+done
