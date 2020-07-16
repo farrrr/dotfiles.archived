ec009f21b6dc66c2b84f5da216c9b4db9e2baec2
Far Tseng <farrrr@gmail.com>
2020-07-16 10:55:58 +0800

    Update dot_myzsh/plugins/common-aliases/common-aliases.plugin.zsh

--- dot_myzsh/plugins/common-aliases/common-aliases.plugin.zsh
+++ dot_myzsh/plugins/common-aliases/common-aliases.plugin.zsh
@@ -47,6 +47,7 @@ export MANPAGER="less -X"
 # path
 pathmunge /usr/local/sbin
 pathmunge /usr/local/opt/openssl/bin
+pathmunge ~/.local/bin
 
 # Make zsh know about hosts already accessed by SSH
 zstyle -e ':completion:*:(ssh|scp|sftp|rsh|rsync):hosts' hosts 'reply=(${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) /dev/null)"}%%[# ]*}//,/ })'
